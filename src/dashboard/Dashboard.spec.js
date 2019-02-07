124d42ff194fbc17ae2a577ba94cb43feec533ab
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-02-06 20:18:16 -0500

    made that tests for the dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,21 @@
-// Test away
+import React from 'react';
+import 'jest-dom/extend-expect'
+import { render, cleanup, fireEvent } from 'react-testing-library'
+
+import Dashboard from './Dashboard'
+
+afterEach(cleanup)
+
+describe('checking the dashboard', () => {
+  describe('checks that both display and controls render', () => {
+    test('renders display', () => {
+      const { getByText } = render(<Dashboard />);
+      getByText('Open')
+    });
+
+    test('renders controls', () => {
+      const { getByText } = render(<Dashboard />)
+      getByText('Close Gate')
+    });
+  });
+})
